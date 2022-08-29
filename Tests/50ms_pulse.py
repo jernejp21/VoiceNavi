a17df7ea24da76d8d9226b7e84cbcd047a05dd1a
jernejp21 <jernej.pangerc@tutamail.com>
2022-08-29 16:37:15 +0900

    test: updated 50ms pulse test case.

--- Tests/50ms_pulse.py
+++ Tests/50ms_pulse.py
@@ -54,7 +54,7 @@ time.sleep(2)
 pulseTime = c_double(0.050)  # 50 ms
 SW1 = c_int(7)
 SW2 = c_int(6)
-SW3 = c_int(7)
+SW3 = c_int(5)
 SW4 = c_int(4)
 SW5 = c_int(3)
 SW6 = c_int(2)
@@ -71,16 +71,27 @@ BUSY = c_int(10)
 HIGH = c_int(2)
 LOW = c_int(1)
 
-for channel in range(0, 12):
+for channel in range(0, 16):
     # 1=DwfDigitalOutIdleLow, low when not running
     dwf.FDwfDigitalOutIdleSet(hdwf, c_int(channel), HIGH)
 
 dwf.FDwfDigitalOutRunSet(hdwf, pulseTime)  # pulseTime run
 dwf.FDwfDigitalOutRepeatSet(hdwf, c_int(1))  # once
 dwf.FDwfDigitalOutCounterInitSet(hdwf, SW1, c_int(0), c_int(0))  # initialize low on start
+#dwf.FDwfDigitalOutCounterInitSet(hdwf, SW3, c_int(0), c_int(0))  # initialize low on start
+#dwf.FDwfDigitalOutCounterInitSet(hdwf, OP, c_int(0), c_int(0))  # initialize low on start
+dwf.FDwfDigitalOutCounterInitSet(hdwf, STOP, c_int(1), c_int(1))  # initialize low on start
+
 # low/high count zero, no toggle during run
 dwf.FDwfDigitalOutCounterSet(hdwf, SW1, c_int(0), c_int(0))
 dwf.FDwfDigitalOutEnableSet(hdwf, SW1, c_int(1))
+#dwf.FDwfDigitalOutCounterSet(hdwf, SW3, c_int(0), c_int(0))
+#dwf.FDwfDigitalOutEnableSet(hdwf, SW3, c_int(1))
+#dwf.FDwfDigitalOutCounterSet(hdwf, OP, c_int(0), c_int(0))
+#dwf.FDwfDigitalOutEnableSet(hdwf, OP, c_int(1))
+
+dwf.FDwfDigitalOutEnableSet(hdwf, STOP, c_int(1))
+dwf.FDwfDigitalOutCounterSet(hdwf, STOP, c_int(0), c_int(0))
 
 '''oscilo start'''
 hzDI = c_double()
@@ -126,27 +137,28 @@ while testNr < numberOfTests:
         if sts.value == DwfStateDone.value:  # done
             break
 
-    dwf.FDwfDigitalOutEnableSet(hdwf, STOP, c_int(1))
-    time.sleep(0.5)
-    dwf.FDwfDigitalOutEnableSet(hdwf, STOP, c_int(0))
+    time.sleep(0.2)
+    dwf.FDwfDigitalOutIdleSet(hdwf, STOP, LOW)
+    time.sleep(0.2)
+    dwf.FDwfDigitalOutIdleSet(hdwf, STOP, HIGH)
 
     # get samples, byte size
     dwf.FDwfDigitalInStatusData(hdwf, rgbSamples, 2*cSamples)
     data = numpy.fromiter(rgbSamples, dtype=numpy.uint16)
 
     sw1_data = data & (1 << 7)
-    sw2_data = data & (1 << 6)
-    op_data = data & (1 << 10)
+    sw2_data = data & (1 << 5)
+    busy_data = data & (1 << 10)
 
     sw1_data = sw1_data >> 7
-    sw2_data = sw2_data >> 6
-    op_data = op_data >> 10
+    sw2_data = sw2_data >> 5
+    busy_data = busy_data >> 10
 
     #T = cSamples/freq
     #t = numpy.linspace(-T/4, T*3/4, cSamples)
     #plt.plot(t, sw1_data, 'r')
     #plt.plot(t, sw2_data, 'g')
-    #plt.plot(t, op_data, 'b')
+    #plt.plot(t, busy_data, 'b')
     #plt.show()
 
     # Check test results
@@ -154,7 +166,7 @@ while testNr < numberOfTests:
         #ok
         #print("SW1 puls detected")
         swDetectResult +=1
-    if(op_data.min() == 0 and op_data.max() == 1):
+    if(busy_data.min() == 0 and busy_data.max() == 1):
         #ok
         #print("OP signal detected")
         opDetectResult +=1
@@ -162,6 +174,7 @@ while testNr < numberOfTests:
     testNr += 1
 
 print()
+print("Pulse length: {0}".format(pulseTime))
 print("SW1 puls detected {0} times".format(swDetectResult))
 print("OP signal detected {0} times".format(opDetectResult))
 print()
