d34c5749c299bc03322e03f0cdd89192ccb6949a
jernejp21 <jernej.pangerc@tutamail.com>
2023-12-10 11:16:50 +0100

    Added Modified hardware

--- generate_chlog.sh
+++ generate_chlog.sh
@@ -1,1 +1,1 @@
-github_changelog_generator -u AZURTEST-CORP -p VoiceNavi -t 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 --output Changelog.md
+github_changelog_generator -u AZURTEST-CORP -p VoiceNavi -t <token> --output Changelog.md --since-tag v3.8
